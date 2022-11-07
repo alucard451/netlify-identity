541a362b2be9eae121b5cc64158a202360c39c30
Sebas <sebastian310803jara@gmail.com>
2022-11-06 20:06:34 -0500

    cambios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { IdentityContextProvider } from 'react-netlify-identity'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const url = "https://playful-mousse-b92e06.netlify.app/.netlify/identity";
+  return (
+  <IdentityContextProvider url={url}>
+
+  <Component {...pageProps} />
+  </IdentityContextProvider>)
 }
---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useIdentityContext } from "react-netlify-identity";
+export default function Home() {
+  const { loginUser, signupUser } = useIdentityContext();
+  const formRef = React.useRef();
+  const [msg, setMsg] = React.useState("");
+
+  const signup = () => {
+    const email = formRef.current.email.value;
+    const password = formRef.current.password.value;
+
+    signupUser(email, password, Object, true)
+      .then((user) => {
+        console.log("Success! Signed up", user);
+      })
+      .catch(
+        (err) => console.error("error", err) || setMsg("Error: " + err.message)
+      );
+  };
+  return (
+    <form
+      ref={formRef}
+      onSubmit={(e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        load(loginUser(email, password, true))
+          .then((user) => {
+            console.log("Success! Logged in", user);
+            navigate("/dashboard");
+          })
+          .catch(
+            (err) => console.error(err) || setMsg("Error: " + err.message)
+          );
+      }}
+    >
+      <div>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+      </div>
+      <div>
+        <input type="submit" value="Log in" />
+        <button onClick={signup}>Sign Up </button>
+        {msg && <pre>{msg}</pre>}
+      </div>
+    </form>
+  );
+}
